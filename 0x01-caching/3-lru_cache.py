f3a4ddbb8ba1d995c4b36e082e0521eb17ea079d
billwk254 <billkihungu51@gmail.com>
2024-02-05 17:02:36 +0300

    a class LRUCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""LRUCache module"""
+
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUCache class"""
+
+    def __init__(self):
+        """Initialize LRUCache"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                discarded_key = self.queue.pop(0)
+                del self.cache_data[discarded_key]
+                print("DISCARD:", discarded_key)
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is not None:
+            if key in self.cache_data:
+                self.queue.remove(key)
+                self.queue.append(key)
+                return self.cache_data[key]
+        return None
+
+
+if __name__ == "__main__":
+    my_cache = LRUCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    print(my_cache.get("B"))
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
+    my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
+    my_cache.put("H", "H")
+    my_cache.print_cache()
+    my_cache.put("I", "I")
+    my_cache.print_cache()
+    my_cache.put("J", "J")
+    my_cache.print_cache()
+    my_cache.put("K", "K")
+    my_cache.print_cache()
