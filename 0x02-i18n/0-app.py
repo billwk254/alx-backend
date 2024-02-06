afab541b48db390942a7d0b738fb88b5c9278f31
billwk254 <billkihungu51@gmail.com>
2024-02-06 16:28:06 +0300

    a basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module defines a basic Flask app.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Render the index.html template.
+    """
+    return render_template('0-index.html', title='Welcome to Holberton', header='Hello world')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,11 @@
+# Basic Flask App with Babel Integration
+
+This repository contains a basic Flask web application with Babel integration. The app serves a single route ("/") and renders an HTML template that displays a welcome message in English.
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/billwk254/alx-backend/0x02-i18n
+cd alx-backend/0x02-i18n
