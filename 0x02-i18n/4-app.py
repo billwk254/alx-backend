34cacff9d1acf50bc8b758c8661850dd1df8a9a9
billwk254 <billkihungu51@gmail.com>
2024-02-06 18:09:23 +0300

    In your get_locale function, detect if the incoming request contains locale argument

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module defines a Flask app with Babel integration.
+This module defines a Flask app with Babel integration and locale parameter support.
 """
 
 from flask import Flask, render_template, request
